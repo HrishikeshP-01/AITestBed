570c5313475f9cd502616b28457113a27baaec0c
Hrishikesh <hrishikeshpramodnair@gmail.com>
2024-11-16 13:28:55 +0530

    Created base state class

---
+++ Plugins/StateMachine/Source/StateMachine/Classes/FSM_State.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "FSM_State.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STATEMACHINE_API UFSM_State : public UDataAsset
+{
+	GENERATED_BODY()
+	
+};
