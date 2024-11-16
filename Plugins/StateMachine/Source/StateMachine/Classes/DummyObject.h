1a0c5168ab5bff78f3b76e157cf7f6a4c21774e2
Hrishikesh <hrishikeshpramodnair@gmail.com>
2024-11-16 13:16:34 +0530

    Print msg when plugin is loaded, created dummy class

---
+++ Plugins/StateMachine/Source/StateMachine/Classes/DummyObject.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "DummyObject.generated.h"
+
+/**
+ * Just a dummy object. Doesn't even have a class file
+ */
+UCLASS()
+class UDummyObject : public UObject
+{
+	GENERATED_BODY()
+	
+};
--- Plugins/StateMachine/Source/StateMachine/Private/StateMachine.cpp
+++ Plugins/StateMachine/Source/StateMachine/Private/StateMachine.cpp
@@ -7,6 +7,7 @@
 void FStateMachineModule::StartupModule()
 {
 	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+	UE_LOG(LogTemp, Log, TEXT("State Machine Module Loaded!"));
 }
 
 void FStateMachineModule::ShutdownModule()
